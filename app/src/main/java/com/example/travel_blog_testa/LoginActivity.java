8a6a8970a14cba26a7469b211d4fa5a501ef615c
bperdomo2016 <bperdomo2016@fau.edu>
2021-12-20 00:21:26 -0500

    added input validation and error messages

--- app/src/main/java/com/example/travel_blog_testa/LoginActivity.java
+++ app/src/main/java/com/example/travel_blog_testa/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.travel_blog_testa;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
@@ -11,7 +12,7 @@ import com.google.android.material.textfield.TextInputLayout;
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputLayout textUsernameLayout;
-    private TextInputLayout textPasswordLayout;
+    private TextInputLayout textPasswordInput;
     private Button loginButton;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,9 +21,40 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login); //connects LoginActivity to activity_login.xml
 
         //binding views from xml to java objects using "findViewById()" method
-        textUsernameLayout = findViewById(R.id.textUsernameLayout;
-        textPasswordLayout = findViewById(R.id.textPasswordInput;
-        loginButton = findViewById(R.id.loginButton;
+        textUsernameLayout = findViewById(R.id.textUsernameLayout);
+        textPasswordInput = findViewById(R.id.textPasswordInput);
+        loginButton = findViewById(R.id.loginButton);
+
+
+        loginButton = findViewById(R.id.loginButton);
+        //setting click listener on login button using setOnCLickListener
+        //OnClickListener() is an interface
+        //When login button is clicked, onLoginClicked() is executed
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.onLoginClicked();
+            }
+        });
+
+
+    }
+
+    //validation and error messages
+    private void onLoginClicked() {
+        //not imp yet
+        //getEditText retrieves reference to input field
+        //getText retrieves actual text from input field that user types
+        String username = textUsernameLayout.getEditText().getText().toString();
+        String password = textPasswordInput.getEditText().getText().toString();
+
+        if (username.isEmpty()) {
+            textUsernameLayout.setError("Username must not be empty");
+
+        } else if (password.isEmpty()) {
+            textPasswordInput.setError("Password must not be empty");
+        }
 
     }
 

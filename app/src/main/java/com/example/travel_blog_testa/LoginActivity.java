04570b37a25f81c01332d4ecce72c65916275524
bperdomo2016 <bperdomo2016@fau.edu>
2021-12-19 23:51:56 -0500

    init login activity

---
+++ app/src/main/java/com/example/travel_blog_testa/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.travel_blog_testa;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}

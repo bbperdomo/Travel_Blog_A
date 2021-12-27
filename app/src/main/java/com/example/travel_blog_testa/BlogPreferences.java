920afbcb72406279c741bab2fe11d5023ee30fd1
bperdomo2016 <bperdomo2016@fau.edu>
2021-12-27 13:20:39 -0500

    new class that holds shared preferences

---
+++ app/src/main/java/com/example/travel_blog_testa/BlogPreferences.java
@@ -0,0 +1,11 @@
+package com.example.travel_blog_testa;
+
+import android.content.SharedPreferences;
+
+public class BlogPreferences {
+
+    private static final String KEY_LOGIN_STATE = "key_login_state";
+
+    private SharedPreferences preferences;
+
+}

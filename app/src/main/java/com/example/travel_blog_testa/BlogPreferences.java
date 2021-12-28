6058d465818e686ce7943a175684bc333a01c5f7
bperdomo2016 <bperdomo2016@fau.edu>
2021-12-28 11:27:35 -0500

    created constructor, getter, and setter methods for shared preferences

--- app/src/main/java/com/example/travel_blog_testa/BlogPreferences.java
+++ app/src/main/java/com/example/travel_blog_testa/BlogPreferences.java
@@ -1,5 +1,6 @@
 package com.example.travel_blog_testa;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 
 public class BlogPreferences {
@@ -7,5 +8,27 @@ public class BlogPreferences {
     private static final String KEY_LOGIN_STATE = "key_login_state";
 
     private SharedPreferences preferences;
+
+    //Constructor - we create/get shared preferences
+    BlogPreferences(Context context) {
+        preferences =
+                //getSharedPreferences is an android method
+                //it has 2 params: name of app, and mode. private mode means only our app has access to these shared preferences
+                context.getSharedPreferences("travel_blog_testa", Context.MODE_PRIVATE);
+    }
+
+    //retrieves the value from shared preferences
+    public boolean isLoggedIn() {
+        //getBoolean is one of shared preferences' get methods
+        //key_login_state is the name of our preference
+        return preferences.getBoolean(KEY_LOGIN_STATE, false);
+    }
+
+    //sets the value into shared preferences
+    public void setLoggedIn(boolean loggedIn) {
+        //the preferences.edit() method returns the "Editor" object, and this object has several put methods available.
+        //apply() stores the data in memory to asynchronously persist on the file system.
+        preferences.edit().putBoolean(KEY_LOGIN_STATE, loggedIn).apply();
+    }
 
 }

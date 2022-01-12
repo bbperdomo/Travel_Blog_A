fa866a6e310afe2c3579d1a3195c6488623be68b
bperdomo2016 <bperdomo2016@fau.edu>
2022-01-12 01:07:38 -0500

    holds author info

---
+++ app/src/main/java/com/example/travel_blog_testa/http/Author.java
@@ -0,0 +1,17 @@
+package com.example.travel_blog_testa.http;
+
+//An object that holds a blog author's information
+
+public class Author {
+
+    private String name;
+    private String avatar;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+}
